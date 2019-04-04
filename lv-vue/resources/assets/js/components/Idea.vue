61b28161e39b5e48c0d7a3c479539061db9d1c13
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-04 14:31:02 -0300

    Configuraçãodo axios

---
+++ lv-vue/resources/assets/js/components/Idea.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="">
+<h2 class="text-center">Escreva suas Ideias</h2>
+        <div class="well">
+            <h4>No que está pensando?</h4>
+            <form>
+                <div class="input-group">
+                    <input type="text" name="" class="form-control input-sm" name="" maxlength="256">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-primary btn-sm">Adicionar</button>
+                    </span>
+                </div>
+            </form>
+            <hr>
+            <ul class="list-un">
+                <li v-for="idea in ideas">
+                    <p>
+                        <small class="text-muted"><em>{{idea.created_at}}</em>
+                        </small>
+                        {{idea.description}}
+                    </p>
+                </li>
+            </ul>
+            
+        </div>
+        </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        data(){
+            return{
+                ideas: [],
+            }
+        },
+        created: function(){
+          this.getIdeas();  
+        },
+        methods: {
+            getIdeas: function(){
+                var  urlIdeas = 'mis-ideas';
+                axios.get(urlIdeas).then(response=>{
+                    this.ideas = response.data
+                });
+            }
+        }
+
+    }
+</script>
+
+<style>
+    
+</style>
--- lv-vue/resources/views/home.blade.php
+++ lv-vue/resources/views/home.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+       
         <title>Laravel - Componentes Vue</title>
 
         
@@ -15,38 +16,17 @@
             <div class="row">
                 <div class="col-xs-8 col-xs-offset-2">
                     <div id="app">
-                        <h2 class="text-center">Escreva suas Ideias</h2>
-                    <div class="well">
-                        <h4>No que está pensando?</h4>
-                        <form>
-                            <div class="input-group">
-                                <input type="text" name="" class="form-control input-sm" name="" maxlength="256">
-                                <span class="input-group-btn">
-                                    <button type="submit" class="btn btn-primary btn-sm">Adicionar</button>
-                                </span>
-                            </div>
-                        </form>
-                        <hr>
-                        <ul class="list-un">
-                            <li>
-                                <p>
-                                    <small class="text-muted"><em>Há um minuto </em>
-                                    </small>
-                                </p>
-                            </li>
-                        </ul>
-                        
+                        <idea></idea>
                     </div>
-                    </div>
-                </div>
+                </div>        
             </div>
         </div>
 
 
-        <script type="text/javascript">
-            
-        </script>
-
-        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+       
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+       
     </body>
 </html>
