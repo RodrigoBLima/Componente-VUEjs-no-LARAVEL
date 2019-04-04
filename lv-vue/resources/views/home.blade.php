2d581d2693b16b27280244d2ef440cfaa932ba84
Rodrigo Barbosa <vanderdigo837@outlook.com>
2019-04-04 13:37:27 -0300

    Vue and Laravel

---
+++ lv-vue/resources/views/home.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+        <title>Laravel - Componentes Vue</title>
+
+        
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-8 col-xs-offset-2">
+                    <div id="app">
+                        <h2 class="text-center">Escreva suas Ideias</h2>
+                    <div class="">
+                        
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    </body>
+</html>
